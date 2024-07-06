2adc95cae33144097b8b6145ed81ecf1257d3244
Yizack Rangel <yizackr@gmail.com>
2024-07-06 02:48:12 -0500

    chore: add compatibility date

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,7 +1,10 @@
 export default defineNuxtConfig({
   modules: ['nuxt-musicfyplayer'],
   devtools: { enabled: true },
+
   imports: {
     autoImport: true,
   },
+
+  compatibilityDate: '2024-07-06',
 })
