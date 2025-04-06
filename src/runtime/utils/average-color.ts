22f20d573dc6497e19c7df347ba027a07bd90ba6
Yizack Rangel <yizackr@gmail.com>
2025-04-06 05:06:59 -0500

    refactor: use `getColorAsync` from `fast-average-color`

--- src/runtime/utils/average-color.ts
+++ src/runtime/utils/average-color.ts
@@ -2,19 +2,7 @@ import { FastAverageColor } from 'fast-average-color'
 
 const fac = new FastAverageColor()
 
-export const getImageAverageColor = (src: string): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const img = new Image()
-    img.crossOrigin = 'Anonymous'
-    img.src = src
-
-    img.onload = () => {
-      const color = fac.getColor(img)
-      resolve(color.rgb)
-    }
-
-    img.onerror = () => {
-      reject(new Error(`Failed to load image: ${src}`))
-    }
-  })
+export const getImageAverageColor = async (src: string) => {
+  const color = await fac.getColorAsync(src)
+  return color.rgb
 }
